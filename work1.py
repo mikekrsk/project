c347df7b337c22f1db40261cf4d4dd3488c220f5
mikekrsk <70261172+mikekrsk@users.noreply.github.com>
2020-08-27 14:25:43 +0700

    Add files via upload

---
+++ work1.py
@@ -0,0 +1,17 @@
+# Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+
+first = 1
+second = first + 4
+text = "пробный текст"
+
+print(first)
+print(second)
+print(text)
+
+zapros_texta = input("Введите текст: ")
+number1 = input("Введите первое число: ")
+number2 = input("Введите второе число: ")
+
+print(zapros_texta)
+print(number1, number2)
